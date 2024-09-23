8273faef9c1a9242a58895dd98da013f4af5378d
rizkyperdananst <riskiperdana345@gmail.com>
2024-09-23 13:59:08 +0700

    string

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Rizky")
+	fmt.Println("Rizky Perdana")
+	fmt.Println("Rizky Perdana Nst")
+
+	fmt.Println(len("Rizky"))
+	fmt.Println("Rizky Perdana"[0])
+	fmt.Println("Rizky Perdana Nst"[1])
+}
