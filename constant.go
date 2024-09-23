e369891f02f94f575e9dbbd870182f6adc19dbde
rizkyperdananst <riskiperdana345@gmail.com>
2024-09-23 15:31:00 +0700

    constant

---
+++ constant.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	const firstName string = "Rizky"
+	const lastName = "Nasution"
+	const value = 1000
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
