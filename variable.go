81cb1933f82b9abfff8d2695ee247ef7575c9444
rizkyperdananst <riskiperdana345@gmail.com>
2024-09-23 14:58:50 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Rizky Perdana Nasution"
+	fmt.Println(name)
+
+	var friendName = "Kaja"
+	fmt.Println(friendName)
+
+	// var age int8 = 22
+	var age = 22
+	fmt.Println(age)
+
+	country := "Indonesia"
+	fmt.Println(country)
+
+	var (
+		firstName = "Rizky"
+		lastName = "Nasution"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
